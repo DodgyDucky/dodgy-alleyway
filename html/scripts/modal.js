281811331399d7daa763f242351109b522e70db1
DodgyDucky <33293215+DodgyDucky@users.noreply.github.com>
2024-11-18 07:21:22 +0100

    .

--- html/scripts/modal.js
+++ html/scripts/modal.js
@@ -2,67 +2,30 @@
 function openModal1() 
 {document.getElementById('myModal1').style.display = "block";}
 
-/*Close*/
-function closeModal1() 
-{document.getElementById('myModal1').style.display = "none";}
-
-var slideIndex = 1;
-showSlides1(slideIndex);
-
-/*Controls*/
-function plusSlides(n)
-{showSlides1(slideIndex += n);}
-
-function currentSlide(n) 
-{showSlides1(slideIndex = n);}
-
-function showSlides1(n) 
-{
-    var i;
-    var slides = document.getElementsByClassName("mySlides1");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) 
-    {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) 
-    {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
-}
-
-
-
-
-
-/*Open*/
-function openModal1() 
+function openModal2() 
 {document.getElementById('myModal2').style.display = "block";}
 
 /*Close*/
 function closeModal1() 
+{document.getElementById('myModal1').style.display = "none";}
+
+function closeModal2() 
 {document.getElementById('myModal2').style.display = "none";}
 
 var slideIndex = 1;
-showSlides2(slideIndex);
+showSlides(slideIndex);
 
 /*Controls*/
 function plusSlides(n)
-{showSlides2(slideIndex += n);}
+{showSlides(slideIndex += n);}
 
 function currentSlide(n) 
-{showSlides2(slideIndex = n);}
+{showSlides(slideIndex = n);}
 
-function showSlides2(n) 
+function showSlides(n) 
 {
     var i;
-    var slides = document.getElementsByClassName("mySlides2");
+    var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("demo");
     var captionText = document.getElementById("caption");
     if (n > slides.length) {slideIndex = 1}
