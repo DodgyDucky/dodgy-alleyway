decb6960e9837b20ab5383cb4d4fe9cabef068f8
DodgyDucky <33293215+DodgyDucky@users.noreply.github.com>
2026-04-09 07:30:43 +0200

    .

--- html/scripts/modals.js
+++ html/scripts/modals.js
@@ -3,13 +3,48 @@ function openModal0() {
   document.getElementById("myModal0").style.display = "block";
 }
 
-function openModal1() {
-  document.getElementById("myModal1").style.display = "block";
-}
-
 // Close the Modal
 function closeModal0() {
   document.getElementById("myModal0").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides0");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+
+
+
+function openModal1() {
+  document.getElementById("myModal1").style.display = "block";
 }
 
 function closeModal1() {
@@ -31,7 +66,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("mySlides1");
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
